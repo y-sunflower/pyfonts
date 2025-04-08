b8039ff42d5b8cf41954db3c2b3eb6ef43dc3209
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-04-08 17:02:17 +0200

    temptative to fix windows issue

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,5 +1,3 @@
-import os
-import pytest
 from pyfonts import load_font, clear_pyfonts_cache
 
 
@@ -11,16 +9,9 @@ def test_print_message_is_valid(capsys):
     clear_pyfonts_cache()
     captured = capsys.readouterr()
 
-    if not os.name == "nt":
-        assert captured.out.strip().startswith("Font cache cleaned: ")
+    assert captured.out.strip().startswith("Font cache cleaned: ")
 
-        # clean twice the cache leads to a different message
-        clear_pyfonts_cache()
-        captured = capsys.readouterr()
-        assert (
-            captured.out.strip() == "No font cache directory found. Nothing to clean."
-        )
-
-    else:
-        with pytest.raises(OSError):
-            clear_pyfonts_cache()
+    # clean twice the cache leads to a different message
+    clear_pyfonts_cache()
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "No font cache directory found. Nothing to clean."
