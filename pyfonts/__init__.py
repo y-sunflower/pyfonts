19f1ea40abe0c658299d2d96bbfb98da59d3365e
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-04-21 16:10:10 +0200

    new version

--- pyfonts/__init__.py
+++ pyfonts/__init__.py
@@ -3,5 +3,5 @@ from .google import load_google_font
 from .cache import clear_pyfonts_cache
 from .preview_font import preview_font
 
-__version__ = "0.3.0"
+__version__ = "1.0.0"
 __all__ = ["load_font", "load_google_font", "preview_font", "clear_pyfonts_cache"]
--- sandbox.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-from pyfonts import load_google_font
-
-font = load_google_font("Roboto", italic=True)  # default Roboto font
-
-fig, ax = plt.subplots()
-ax.text(x=0.2, y=0.3, s="Hey there!", size=30, font=font)
