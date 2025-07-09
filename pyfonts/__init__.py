9752d608409e408997182c5b0b3f1aaa0ffeea91
John Gardner <gardner.john97+dphil@gmail.com>
2025-07-09 11:09:34 +0100

    set default font

--- pyfonts/__init__.py
+++ pyfonts/__init__.py
@@ -1,4 +1,4 @@
-from .main import load_font
+from .main import load_font, set_default
 from .google import load_google_font
 from .cache import clear_pyfonts_cache
 from .preview_font import preview_font
@@ -7,6 +7,7 @@ __version__ = "1.0.0"
 __all__ = [
     "load_font",
     "load_google_font",
+    "set_default",
     "preview_font",
     "clear_pyfonts_cache",
 ]
