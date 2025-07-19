3886fc2d85a7a6d8592150da31ca4f5220e01115
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-07-19 10:01:33 +0200

    Release v1.1.0

--- pyfonts/__init__.py
+++ pyfonts/__init__.py
@@ -3,7 +3,7 @@ from .google import load_google_font
 from .cache import clear_pyfonts_cache
 from .preview_font import preview_font
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __all__ = [
     "load_font",
     "load_google_font",
