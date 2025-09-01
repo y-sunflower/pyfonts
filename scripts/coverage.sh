45b14c1db6c6350fcdf0a7fe095255d2dd39cb4b
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-09-01 10:34:28 +0200

    use makefile

--- scripts/coverage.sh
+++
@@ -1,5 +0,0 @@
-uv run coverage run --source=pyfonts -m pytest
-uv run coverage report -m
-uv run coverage xml
-PYTHONWARNINGS="ignore::UserWarning" uv run genbadge coverage -i coverage.xml
-rm coverage.xml
